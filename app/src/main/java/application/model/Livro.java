c27b6eff96ffc515f6cc284086379cc4f56e4bf9
KittyMoon Dias <gui.augusto32@gmail.com>
2023-10-06 01:22:04 +0000

    Salvar

--- app/src/main/java/application/model/Livro.java
+++ app/src/main/java/application/model/Livro.java
@@ -7,56 +7,53 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
-import application.model.Autor;
-import application.model.Genero;
 
 @Entity
-@Table(name="Livros")
+@Table(name = "Livros")
 public class Livro {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String titulo;
 
-
-
     @ManyToOne
-    @JoinColumn(name="id_genero")
+    @JoinColumn(name = "id_genero")
     private Genero genero;
-    public void set Genero (Genero, genero){
-        this.genero(genero);
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
     }
-    public Genero.getGenero(){
-        return.this.genero;
+
+    public Genero getGenero() {
+        return this.genero;
     }
 
     @ManyToOne
-    @JoinColumn(name="id_autor")
+    @JoinColumn(name = "id_autor")
     private Autor autor;
-    public void set Autor (Autor, autor){
-        this.autor(autor);
-    }
-    public Autor.getAutor(){
-        return.this.autor;
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
     }
 
-    public void setId(int Id){
+    public Autor getAutor() {
+        return this.autor;
+    }
+
+    public void setId(int id) {
         this.id = id;
-    }public int getId(){
+    }
+
+    public int getId() {
         return id;
     }
 
-    public int getId(){
-        return id;
+    public String getTitulo() {
+        return titulo;
     }
-    public void setId(int id){
-        this.id = id;
-    }
-    public String getTitulo(){
-        return nome;
-    }
-    public void setNome(String nome){
-        this.nome = nome;
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 }
 
